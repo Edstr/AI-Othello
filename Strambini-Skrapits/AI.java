22b4a336cca901b20347fa49580b42af0b57bc08
Edstr <ed.strambini@gmail.com>
2014-11-26 13:42:37 +0100

    Commit Initial

---
+++ Strambini-Skrapits/AI.java
@@ -0,0 +1,16 @@
+package Participants.Strambini-Skrapits;
+
+import Othello.Move;
+
+public class AI
+{
+	public Move getBestMove(GameBoard gameBoard, int depth, int playerID)
+	{
+		
+	}
+	
+	private double alphabeta(GameBoard gameboard, int depth, int minOrMax, 
+	{
+		
+	}
+}
