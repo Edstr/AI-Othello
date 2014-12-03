3b9144a121baff289d81f89e8ab5752e7d1e6601
Edstr <ed.strambini@gmail.com>
2014-12-03 11:47:20 +0100

    Ajout du tableau des valeurs des cases du jeu.

--- Strambini-Skrapits/State.java
+++ Strambini-Skrapits/State.java
@@ -8,6 +8,13 @@ public class State
 	private State parent;
 	private int playerID;
 	private int enemyID;
+	
+	private static int[][] force ={
+		{99,-8,8,6},
+		{-8,-24,-4,-3},
+		{8,-4,7,4},
+		{6,-3,4,0}
+	};
 
 	public State(State parent, int playerID, GameBoard gameBoard) {
 		this.parent = parent;
@@ -33,5 +40,21 @@ public class State
 
 	public void apply(Move move) {
 		this.gameBoard.addCoin(move, this.playerID);
+	}
+	
+	// Méthode retournant la force d'une case.
+	public static int[] getForce(int posI, int posJ)
+	{
+		if(posI >= 4)
+		{
+			posI = 7 % posI;
+		}
+		if(posJ >= 4)
+		{
+			posJ = 7 % posJ;
+		}		
+		int []pos = {posI,posJ};	
+		
+		return pos;
 	}
 }
